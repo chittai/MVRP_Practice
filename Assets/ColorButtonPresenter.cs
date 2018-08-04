69d2a6b09fbd8f4400d9957bdfcb4563443e7f39
taquan <chittai1221@gmail.com>
2018-08-05 06:51:45 +0900

    FirstCommit

---
+++ Assets/ColorButtonPresenter.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using UnityEngine.UI;
+
+public class ColorButtonPresenter : MonoBehaviour {
+
+    [SerializeField] Button redButton;
+    [SerializeField] Button blueButton;
+    [SerializeField] Button yellowButton;
+
+    void Start () {
+        var colorConfig = ColorConfigComponent.Instance;
+
+        var material = GetComponent<Renderer>().material;
+
+        var redColorStream = redButton.OnClickAsObservable().Select(_=> Color.red);
+        var blueColorStream = blueButton.OnClickAsObservable().Select(_ => Color.blue);
+        var yellowColorStream = yellowButton.OnClickAsObservable().Select(_ => Color.yellow);
+
+        ///<Summary>
+        ///Model → View への反映
+        ///</Summary>
+        colorConfig.colorConfig
+            .Subscribe(c => material.color = c);
+
+        ///<Summary>
+        ///View → Model への反映
+        ///</Summary>
+        redColorStream
+            .Subscribe(c => colorConfig.colorConfig.Value = c );
+
+        blueColorStream
+            .Subscribe(c => colorConfig.colorConfig.Value = c);
+
+        yellowColorStream
+            .Subscribe(c => colorConfig.colorConfig.Value = c);
+    }
+}
---
+++ Assets/ColorConfigComponent.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+
+public class ColorConfigComponent : MonoBehaviour {
+
+    private ColorConfigComponent() {}
+
+    public ReactiveProperty<Color> colorConfig = new ReactiveProperty<Color>();
+
+    public static ColorConfigComponent Instance
+    {
+        get
+        {
+            var instance = new ColorConfigComponent();  
+            return instance;
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
